d3f5c76d22b0b22b06f1bbfb0838a90d52ed10fb
Skinken123 <guma.moritz@gmail.com>
2024-08-31 14:22:57 +0200

    test

--- RandomAccessA1.java
+++ RandomAccessA1.java
@@ -7,11 +7,10 @@ public class RandomAccessA1 {
     public static void main(String[] args) {
         int[] sizes = {100, 200, 400, 800, 1600, 3200, 6400, 12800, 25600}; //array som ska accessas
         int loop = 10000; // så många gånger som array ska accessas vid ett random index 1 till n
-        int k = 1000; // så många gånger benchmark ska köras
+        int k = 10000; // så många gånger benchmark ska köras
         int formatting = 1;
         String f = "";
 
-        bench(1000, 1000000); //warmup
         for(int n : sizes) {
             long min = Long.MAX_VALUE;
             long max = 0;
